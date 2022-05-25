d0d66e9cced62b9986e01cb64702e98d81bc7041
gutosenna <ZXC321zxc321>
2022-05-25 19:05:25 -0300

    Palpites para a Mega Sena

--- ex88.py
+++ ex88.py
@@ -1,4 +1,4 @@
-#Exercício Python 088: Faça um programa que ajude um jogador da MEGA SENA a criar palpites.
+# Exercício Python 088: Faça um programa que ajude um jogador da MEGA SENA a criar palpites.
 # O programa vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60 para cada jogo,
 # cadastrando tudo em uma lista composta.
 from random import randint as rd
@@ -23,8 +23,8 @@ while tot < resp:
     sena.append(lista[:])
     lista.clear()
     tot += 1
-print('-=' * 3, f' SORTEANDO {resp} JOGOS ','-=' * 3)
-#for c in range(len(sena)):
+print('-=' * 3, f' SORTEANDO {resp} JOGOS ', '-=' * 3)
+# for c in range(len(sena)):
 #    sleep(0.8)
 #    print(f'Jogo {c+1}: {sena[c]}')
 for i, l in enumerate(sena):
