aa05277ff2d4492a4f3f2b83905734428dc8c4f6
gutosenna <ZXC321zxc321>
2022-06-01 23:47:46 -0300

    Analisando e gerendo Dicionários

---
+++ ex105.py
@@ -0,0 +1,32 @@
+# Exercício Python 105: Analisando e gerendo Dicionários
+# Faça um programa que tenha uma função notas()
+# que pode receber várias notas de alunos e vai retornar um dicionário com as seguintes informações:
+# – Quantidade de notas
+#   – A maior nota
+#       – A menor nota
+#           – A média da turma
+#               – A situação (opcional)
+def notas(*n, sit=False):
+    """
+    -> Função para analisar notas e situações de vários alunos.
+    :param n: uma ou mais notas dos alunos (aceita várias)
+    :param sit: valor opcional, indicando se deve ou não adicionar a situação
+    :return: dicionário com várias informações sobre a situação da turma.
+    """
+    turma = dict()
+    turma['total'] = len(n)
+    turma['maior'] = max(n)
+    turma['menor'] = min(n)
+    turma['média'] = (sum(n)) / len(n)
+    if sit:
+        if turma['média'] >= 7:
+            turma['situação'] = 'BOA'
+        elif turma['média'] >= 5:
+            turma['situação'] = 'RAZOÁVEL'
+        else:
+            turma['situação'] = 'RUIM'
+    return turma
+
+
+resp = notas(1.5, 2.5, 6.5, 5, sit=True)
+print(resp)
