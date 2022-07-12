8e25d21ce991de260374fe30bf5ccc5eb3506f59
gutosenna <79454869+gutosenna@users.noreply.github.com>
2022-07-12 17:00:31 -0300

    Update link_cemig.py

--- link_cemig.py
+++ link_cemig.py
@@ -8,8 +8,10 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-load_dotenv(dotenv_path=r'C:\Users\GUTO SENNA\PycharmProjects\CEMIG\arquivo.env')
-local = r'C:\Users\GUTO SENNA\Downloads'
+load_dotenv(dotenv_path=r'C:\Users\Usuário\PycharmProjects\projetos_servico\CEMIG\arquivo.env')
+local = r'G:\Meu Drive\SEÇÃO DE GESTÃO DE CONTRATOS\CEMIG - 2022\data'
+mes = ''
+error = []
 
 servico = Service(ChromeDriverManager().install())
 navegador = webdriver.Chrome(service=servico)
@@ -24,10 +26,13 @@ matricula = ['3004304247', '3003759292', '3005570849', '3003302106', '3003302105
              '3003586950', '3003303648', '3007648147', '3007547772',
              '3003570759', '3003278251', '3007242702', '3007332376', '3012026982']
 # matricula = ['3004304247', '3003759292']
-temp, lista = [], []
+instList, newInstList = [], []
 try:
     while True:
         for nInst in matricula:
+            error = []
+            error.append(nInst)
+            print(error)
             navegador.find_element(By.CLASS_NAME, 'input').send_keys(nInst)
             navegador.find_element(By.ID, 'pesquisaInstalacaoTop').click()
             sleep(15)
@@ -38,34 +43,39 @@ try:
                     'onclick')
                 texto = navegador.find_element(By.XPATH, f'//*[@id="tblGrid"]/tbody/tr[{i}]/td[1]').text
                 site = link.replace("BaixarPDF('/", "https://atende.cemig.com.br/").replace("', $(this));", "")
-                print(texto, nInst, site)
+                if '07' in texto:
+                    mes = 'Junho'
+                elif '06' in texto:
+                    mes = 'Maio'
                 data = {
                     "Matricula": nInst,
+                    'Mes': mes,
                     'dataVencimento': texto,
                     "link": site
                 }
-                temp.append(data)
-                # print(temp)
-            lista = temp[:]
+                print(mes, texto, nInst, site)
+                instList.append(data)
+                # print(instList)
+            newInstList = instList[:]
+            if len(newInstList) / 2 == len(matricula):
+                break
             navegador.find_element(By.ID, 'dropdownSelecionarInstalacao').click()
             navegador.find_element(By.ID, 'btnSelecionarOutraIN').click()
             sleep(10)
-            if len(lista) / 2 == len(matricula):
-                df = pd.DataFrame(data=lista)
-                path = r"{}".format(local)
-                try:
-                    os.mkdir(path)
-                except FileExistsError:
-                    print(path, 'exits!')
-                else:
-                    print(path, 'created!')
-                finally:
-                    filepath = r"{}\cemig_hist_{}.csv".format(local, time.strftime("%Y%m%d-%H%M%S"))
-                    df.to_csv(filepath, index=False)
-                    print(filepath, 'created!')
-                    navegador.close()
-                break
-        print(lista)
 except:
+    print(f'Parou na matricula {error}')
     pass
-
+finally:
+    print(len(newInstList), newInstList)
+    df = pd.DataFrame(data=newInstList)
+    path = r"{}".format(local)
+    try:
+        os.mkdir(path)
+    except FileExistsError:
+        print(path, 'exits!')
+    else:
+        print(path, 'created!')
+    filepath = r"{}\cemig_hist_{}.csv".format(local, time.strftime("%Y%m%d-%H%M%S"))
+    df.to_csv(filepath, index=False)
+    print(filepath, 'created!')
+    navegador.close()
